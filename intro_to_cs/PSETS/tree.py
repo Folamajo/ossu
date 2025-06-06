dd46f0de830ef308355b3c2a80e21c054f8cf980
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-06-06 06:36:26 +0100

    Fixed class in trre.py

--- intro_to_cs/PSETS/tree.py
+++ intro_to_cs/PSETS/tree.py
@@ -1,5 +1,9 @@
+from typing import Optional
+
 class Node:
-   def __init__(self, value:int, left_child = None, right_child = None):
+   def __init__(self, value:int, left_child: Optional['Node'], right_child: Optional['Node']):
       self.value = value
       self.left_child = left_child
       self.right_child = right_child
+
+   
