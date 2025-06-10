bf61eb23b88d229c65c6b53850e6e4850aa462d0
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-06-10 08:00:06 +0100

    Working on Tree and Nodes

--- intro_to_cs/PSETS/tree.py
+++ intro_to_cs/PSETS/tree.py
@@ -1,9 +1,20 @@
 from typing import Optional
 
 class Node:
-   def __init__(self, value:int, left_child: Optional['Node'], right_child: Optional['Node']):
+   def __init__(self, value:int , left_child: Optional['Node'] = None, right_child: Optional['Node'] = None):
       self.value = value
       self.left_child = left_child
       self.right_child = right_child
 
+   def get_value(self):
+      return self.value
    
+   def get_left_child(self):
+      return self.left_child
+   
+   def get_right_child(self):
+      return self.right_child
+
+
+tree1 = Node(1, Node(2), Node(5, Node(7), Node(8)))
+print(tree1.get_left_child().get_left_child())
